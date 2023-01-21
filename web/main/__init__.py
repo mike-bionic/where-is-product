1af884bc86e58918843468a63cbb962a1c7a8791
Muhammed Jepbarov <muhammed@millione.tm>
2023-01-21 10:15:44 +0500

    updated static files and folders location

--- web/main/__init__.py
+++ web/main/__init__.py
@@ -14,6 +14,9 @@ login_manager.login_message_category = 'info'
 def create_app(config_class=Config):
 	app = Flask(__name__)
 	app.config.from_object(Config)
+	app.static_folder = Config.STATIC_FOLDER_LOCATION
+	app.template_folder = Config.TEMPLATE_FOLDER_LOCATION
+
 	CORS(app)
 	db.init_app(app)
 	login_manager.init_app(app)
--- web/webui.py
+++ web/webui.py
@@ -5,7 +5,7 @@ tk = Tk()
 
 def runview():
 	tk.geometry('1520x840')
-	webview.create_window("Antiplagiat", "http://localhost:5000/")
+	webview.create_window("Ýaş oýlap tapyjylar", "http://localhost:5000/")
 	webview.start()
 
 runview()
